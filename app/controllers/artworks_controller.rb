0d00dcd933e1322858c619d9281c1de16669c5f0
Reed Van Anrooy <reed.vananrooy@gmail.com>
2020-08-04 19:58:37 -0600

    artwork CRUD

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,7 +1,45 @@
 class ArtworksController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_artwork, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    if @artwork.save
+      redirect_to @artwork
+    else
+      render 'new'
+    end
+  end
 
   def index
     @arts = Artwork.all
+  end
+
+  def update
+    if @artwork.update(artwork_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @artwork.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def set_artwork
+    @artwork = Artwork.find(params[:id])
+  end
+
+  def artwork_params
+    params.require(:artwork).permit(:name, :description, :photo)
   end
 end
